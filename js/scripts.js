ff22aa19a99260c0bf1307e00ea14cdaa3f132c3
Llilian <njambililian93@gmail.com>
2020-02-27 16:52:46 +0300

    add js/scrpits

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-  $("form").submit(function(event){
-    // event.preventDefault();
-    var name = $("input#name1").val();
-    var email = $("input#name0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#name1").val() && $("input#name0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
-    
-  });
 
+$().ready(function(){
+  $("#ink").click(function (){
+    $("#design-images").toggle();
+    $("#design").toggle();
 });
+});
+$().ready(function(){
+  $("#ink1").click(function (){
+    $("#development-images").toggle();
+    $("#development").toggle();
+});
+});
+$().ready(function(){
+  $("#ink2").click(function (){
+    $("#product-images").toggle();
+    $("#product").toggle();
+});
+});
+// $().ready(function(){
+//   $(".ink").click(function (){
+//     $("#project").toggle();
+//     $("#project").toggle();
+// });
