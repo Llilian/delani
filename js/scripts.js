ea56346d8be9d60e981b53a7f971770932efa7e7
Llilian <njambililian93@gmail.com>
2020-03-05 10:41:25 +0300

    add changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
-
-$().ready(function(){
-  $("#ink").click(function (){
-    $("#design-images").toggle();
-    $("#design").toggle();
-});
-});
-$().ready(function(){
-  $("#ink1").click(function (){
-    $("#development-images").toggle();
-    $("#development").toggle();
-});
-});
-$().ready(function(){
-  $("#ink2").click(function (){
-    $("#product-images").toggle();
-    $("#product").toggle();
-});
-});
-$().ready(function(){
-  $(this).("").fadeToogle(1000,"linear");
+$(document).ready(function(){
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1000').hide('800');
+    $("#development").show('1500');
+    $("#development p").show();
+  });
+  $("#development").click(function(){
+    $("#development-image").slideDown('1100');
+    $("#development").slideUp('100');
+  });
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1000').hide('1200');
+    $("#design").show('1000');
+    $("#design p").show();
+  });
+  $("#design").click(function(){
+    $("#design").slideUp('1200');
+    $("#design-image").slideDown('1200');
+  });
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+    $("#product p").show();
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
 });
